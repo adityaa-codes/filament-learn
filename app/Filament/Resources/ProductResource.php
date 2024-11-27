108490981ccf2500ae0c214ecf41a17598680998
Aditya Gupta <mailadityaguptaa@gmail.com>
2024-11-27 14:30:09 +0530

    feat: format price column in table
    - mutate data in form before fill and save

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -30,7 +30,10 @@ class ProductResource extends Resource
         return $table
             ->columns([
                 Tables\Columns\TextColumn::make('name')->sortable()->searchable(),
-                Tables\Columns\TextColumn::make('price')->sortable(),
+                Tables\Columns\TextColumn::make('price')->sortable()
+                    ->money('inr')->getStateUsing(function (Product $record) {
+                    return $record->price / 100;
+                }),
             ])->defaultSort('price','desc')
             ->filters([
                 //
